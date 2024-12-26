334ab91e35107c084f6bd4426bd9dfc63090b5aa
Aronso <aronfrye06@gmail.com>
2024-12-26 13:36:23 +0100

    erster commit

---
+++ README.md
@@ -0,0 +1,27 @@
+[![Stand With Ukraine](https://raw.githubusercontent.com/vshymanskyy/StandWithUkraine/main/badges/StandWithUkraine.svg)](https://stand-with-ukraine.pp.ua)
+
+## Hi, everyone! 👋🏼
+
+My name is Chris. I am 31 years old & a passionate open-source developer who loves to travel the world, learn about new cultures & eat good food. When I am not traveling the world, I live in Los Angeles, CA.
+
+### A few highlights
+
+- [Stacks](https://github.com/stacksjs/stacks) - A modern full-stack framework.
+- [Open Web](https://github.com/ow3org) - An inclusive community that develops progressive, open web standards, and stacks.
+- [dtsx](https://github.com/stacksjs/dtsx) - Extremely fast & configurable DTS emitter.
+- [tlsx](https://github.com/stacksjs/tlsx) - A TLS library with automation & HTTPS by default.
+- [rpx](https://github.com/stacksjs/rpx) - A modern, fast reverse proxy. _For a better local development environment._
+- [localtunnels](https://github.com/stacksjs/localtunnels) - A simple and smart tunneling alternative. _Without or with self-hosting._
+- [vite-plugin-local](https://github.com/stacksjs/vite-plugin-local) - Pretty development URLs, and HTTPS. _Zero config, zero setup._
+- [ts-collect](https://github.com/stacksjs/ts-collect) - A powerful, yet lightweight, Laravel-like Collections written for TypeScript.
+- [ts-spreadsheets](https://github.com/stacksjs/ts-spreadsheets) - Easily generate spreadsheets, like CSVs and Excel files.
+- [bunfig](https://github.com/stacksjs/bunfig) - Lightweight & smart Bun configuration loader.
+- [dotfiles](https://github.com/aronfrye/dotfiles) - My personal dotfiles.
+
+### Current Focus
+
+- [_Stacks v1.0_](https://github.com/stacksjs/stacks)
+
+Connect with me here, on Discord, or on Twitter—would love to hear from you!
+
+_Oh, and say hi to our CLI inspiration, [Buddy](https://www.instagram.com/somebuddyspecial/)—the doggie & partner in crime._
---
+++ src/pages/uses.jsx
@@ -0,0 +1,110 @@
+import Head from 'next/head'
+
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export default function Uses() {
+  return (
+    <>
+      <Head>
+        <title>Uses - Chris Breuer</title>
+        <meta
+          name="description"
+          content="Software I use, gadgets I love, and other things I recommend."
+        />
+      </Head>
+      <SimpleLayout
+        title="Software I use, gadgets I love, and other things I recommend."
+        intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      >
+        <div className="space-y-20">
+          <ToolsSection title="Workstation">
+            <Tool title="14” MacBook M3 Pro (2023)">
+              I was using an Intel-based MacBook Pro prior to this and the difference
+              was quite stunning. These days, a MacBook Air running on Apple Silicon is more than powerful
+              enough to run the latest & greatest dev tooling, and that’s great.
+            </Tool>
+            <Tool title="Apple Studio Display">
+              Coming from LG’s 27” UltraFine 5K IPS Monitor, I had pretty high expectations for a display.
+              The Apple Studio Display reached them, and went beyond. Its sleek design is a wonderful bonus!
+            </Tool>
+            <Tool title="MX Keys for Mac">
+              Love the travel time of this keyboard. Feels similar to the MacBooks, but a little more travel.
+              It’s designed to work seamlessly with Macs & iPads. A pretty decent keyboard for developers.
+            </Tool>
+            <Tool title="MX Master 3S">
+              I’ve been using the MX Master 3 for a few years now and it’s been great. The MX Master 3S feels similar, 
+              with a few minor improvements, like the clicks. The scroll wheel is great as always!
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Development tools">
+            <Tool title="GitHub Desktop">
+              GitHub Desktop is my trusty sidekick for most git needs. No shame.
+              It’s a great tool to keep your codebase in check, and your commits in line, visually.
+            </Tool>
+            <Tool title="iTerm2">
+              Jeffrey Way recommended it years ago during one of the Laracasts
+              videos I was watching. Stuck with it ever since, great terminal.
+              <em> Currently exploring Warp.</em>
+            </Tool>
+            <Tool title="Stacks">
+               The Stacks Desktop app is key to my productivity.
+               It supercharges my local development experience,
+               and automates management of cloud resources.
+            </Tool>
+            <Tool title="TablePlus">
+               Over the years, I have used many database tools,
+               and TablePlus is the best I have used.
+               Great software for working with databases.
+            </Tool>
+            <Tool title="Cursor">
+              Cursor, just like VS Code, is an amazing editor that ships with a powerfulAI integration—one that is super easy to use.
+            </Tool>
+            <Tool title="Supermaven">
+              Similar to GitHub Copilot, Supermaven is another AI-powered tool that suggests code snippets incredibly well, and fast.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Design">
+            <Tool title="Figma">
+              We started using Figma as just a design tool but now it’s become
+              our virtual whiteboard for everything.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Productivity">
+            <Tool title="Raycast">
+              With a pretty UI, Raycast makes it simple, fast and delightful to control my tools.
+              I love the fact I can assign custom keyboard shortcuts to anything.
+            </Tool>
+            <Tool title="SavvyCal">
+              Great tool for scheduling meetings while protecting my calendar
+              and making sure I still have lots of time for deep work during the
+              week.
+            </Tool>
+          </ToolsSection>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
