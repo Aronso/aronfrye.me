6f7f75a0ea8ebfc58b7dc8a124722b2379398604
Aronso <aronfrye06@gmail.com>
2025-01-02 20:01:03 +0100

    x

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -376,9 +376,8 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Schueler, Fussballspieler, Zocker 
+            Schueler, Fussballspieler, Zocker, und Tierliebhaber 🐶
             <br />
-            und Tierliebhaber 🐶
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             Ich bin Aron, ein Schüler des Rupertneudeck-Gymnasiums und komme vom Draum,
