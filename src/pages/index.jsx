a36a8db2ca4d62d4ea9cc86d665fa654388ad62a
Aronso <aronfrye06@gmail.com>
2025-01-04 13:16:10 +0100

    Bilder Update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,11 +12,11 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
-import image1 from '@/images/photos/image-1.jpeg'
-import image2 from '@/images/photos/image-2.jpeg'
-import image3 from '@/images/photos/image-3.jpeg'
-import image4 from '@/images/photos/image-4.jpeg'
-import image5 from '@/images/photos/image-5.jpeg'
+import image1 from '@/images/photos/image-1.jpg'
+import image2 from '@/images/photos/image-2.jpg'
+import image3 from '@/images/photos/image-3.jpg'
+import image4 from '@/images/photos/image-4.jpg'
+import image5 from '@/images/photos/image-5.jpg'
 // import logoEliinova from '@/images/logos/eliinova.png'
 // import logoCion from '@/images/logos/cion.png'
 // import logoBixolon from '@/images/logos/bixolon.png'
